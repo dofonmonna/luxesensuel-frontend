04d779a1860bb6b4da5d2995ada0c88712434ff3
LuxeSensuel <luxesensuel11@gmail.com>
2026-04-16 17:20:01 +0000

    fix: AECallback export nommé

--- src/pages/AECallback.tsx
+++ src/pages/AECallback.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
-export default function AECallback() {
+export function AECallback() {
   const [searchParams] = useSearchParams();
   const code = searchParams.get('code');
 
