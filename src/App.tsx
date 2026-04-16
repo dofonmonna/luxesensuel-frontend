f7bd3b8f804267883f5759bc00addafb0484ea03
LuxeSensuel <luxesensuel11@gmail.com>
2026-04-16 20:49:40 +0000

    fix: remove About import causing 500 error
    - Remove About import and route from App.tsx
    - Fix Vite resolution error

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import { ConfirmReception } from './pages/ConfirmReception';
 import { Login } from './pages/Login';
 import { Signup } from './pages/Signup';
 import { Profile } from './pages/Profile';
-import { About } from './pages/About'; // ← AJOUT ICI
 // Admin
 import { Admin } from './pages/Admin';
 import { AdminLogin } from './pages/AdminLogin';
@@ -37,7 +36,6 @@ export function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/about" element={<About />} /> {/* ← AJOUT ICI */}
           <Route path="/callback" element={<AECallback />} />
         </Route>
 
